428fb3d825a4646df294c7e808868cc83e30fc93
AmberStory <--global>
2019-07-15 09:11:37 +0800

    output management

--- src/asset-management/index.js
+++ src/asset-management/index.js
@@ -1,13 +1,13 @@
 import _ from 'lodash';
 import style from './style.css';
-import img from './img/test.png';
+import img from './img/bg.png';
 
 function component() {
     const el = document.createElement('div');
     const myImg = new Image();
     el.innerHTML = _.join(['你好', '小红'], ' ');
     myImg.src = img;
-    el.appendChild = myImg;
+    el.appendChild(myImg);
     return el;
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,12 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-        publicPath: './dist/'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader'],
-            },
-            {
-                test: /\.(png|jpg|jpeg|svg|gif)$/,
-                use: [
-                    {
-                        loader: 'file-loader',
-                        options: {
-                            limit: 8192,
-                            name: 'images/[name].[ext]',
-                        }
-                    }
-                ],
-            },
-            {
-                test: /\.(ttf|woff)$/,
-                use: ['file-loader']
-            }
-        ],
-    }
+  entry: {
+    app: './src/output-management/index.js',
+    print: './src/output-management/print.js'
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  }
 }
