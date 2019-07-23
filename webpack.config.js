0cb18ea9d48692cbcae812de5b5baefb1fe8f29e
AmberStory <378411799@qq.com>
2019-07-23 09:30:00 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   entry: {
@@ -8,5 +10,17 @@ module.exports = {
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
-  }
+  },
+  devtool: 'inline-source-map',
+  devServer: { contentBase: './dist' },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Output Management',
+      inject: "true",
+      meta: {
+        viewport: 'width=device-width, initial-scale=1, shrink-to-fit=no'
+      }
+    }),
+    new CleanWebpackPlugin(),
+  ]
 }
