9a1729c20ad47e5c867b102f48daee85103144c9
AmberStory <378411799@qq.com>
2019-07-28 11:35:24 +0800

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,19 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
   entry: {
     app: './src/output-management/index.js',
-    print: './src/output-management/print.js'
   },
   output: {
     filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/'
   },
   devtool: 'inline-source-map',
-  devServer: { contentBase: './dist' },
+  devServer: { contentBase: './dist', hot: true },
   plugins: [
     new HtmlWebpackPlugin({
       title: 'Output Management',
@@ -22,5 +23,6 @@ module.exports = {
       }
     }),
     new CleanWebpackPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
   ]
 }
